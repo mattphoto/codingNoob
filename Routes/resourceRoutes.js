dadd0e42c1e783c5a5d9943b4d7f2b4e099c78dc
mattphoto <github@mattkim.com>
2016-04-01 12:55:30 -0700

    start subnav

--- Routes/resourceRoutes.js
+++ Routes/resourceRoutes.js
@@ -6,16 +6,16 @@ var routes = function (Resource) {
 
   resourceRouter.route('/')
 
-    // .post(function (req, res) {
-    //   var resource = new Resource(req.body);
-    //   console.log(resource);
-    //   resource.created_at = new Date();
-    //   resource.updated_at = new Date();
+    .post(function (req, res) {
+      var resource = new Resource(req.body);
+      console.log(resource);
+      resource.created_at = new Date();
+      resource.updated_at = new Date();
 
-    //   console.log(req.body);
-    //   resource.save();
-    //   res.status(201).send(resource);
-    // })
+      console.log(req.body);
+      resource.save();
+      res.status(201).send(resource);
+    })
     .get(function (req, res) {
       var query = {};
       if (req.query.category ) {
@@ -30,75 +30,75 @@ var routes = function (Resource) {
     });
 
 
-  // resourceRouter.use('/:resourceId', function (req, res, next) {
-  //     Resource.findById(req.params.resourceId, function (err, resource) {
-  //       if (err)
-  //         res.status(500).send(err);
-  //       else if (resource) {
-  //         req.resource = resource;
-  //         next();
-  //       } else {
-  //         res.status(404).send('no resource found');
-  //       }
-  //   });
-  // });
+  resourceRouter.use('/:resourceId', function (req, res, next) {
+      Resource.findById(req.params.resourceId, function (err, resource) {
+        if (err)
+          res.status(500).send(err);
+        else if (resource) {
+          req.resource = resource;
+          next();
+        } else {
+          res.status(404).send('no resource found');
+        }
+    });
+  });
 
-  // resourceRouter.route('/:resourceId')
-  //   .get(function (req, res) {
-  //     res.json(req.resource);
-  //   })
-  //   .put(function (req, res) {
-  //     req.resource.rating = req.body.rating;
-  //     req.resource.cost = req.body.cost;
+  resourceRouter.route('/:resourceId')
+    .get(function (req, res) {
+      res.json(req.resource);
+    })
+    .put(function (req, res) {
+      req.resource.rating = req.body.rating;
+      req.resource.cost = req.body.cost;
 
-  //     req.resource.category = req.body.category;
-  //     req.resource.title = req.body.title;
-  //     req.resource.author = req.body.author;
-  //     req.resource.url = req.body.url;
-  //     req.resource.description = req.body.description;
+      req.resource.category = req.body.category;
+      req.resource.title = req.body.title;
+      req.resource.author = req.body.author;
+      req.resource.url = req.body.url;
+      req.resource.description = req.body.description;
 
-  //     req.resource.difficulty = req.body.difficulty;
-  //     req.resource.type = req.body.type;
-  //     req.resource.length = req.body.length;
-  //     req.resource.date = req.body.date;
-  //     req.resource.date_published = req.body.date_published;
-  //     req.resource.updated_at = new Date();
+      req.resource.difficulty = req.body.difficulty;
+      req.resource.type = req.body.type;
+      req.resource.length = req.body.length;
+      req.resource.date = req.body.date;
+      req.resource.date_published = req.body.date_published;
+      req.resource.updated_at = new Date();
 
-  //     console.log(req.resource);
+      console.log(req.resource);
 
-  //     req.resource.save(function (err) {
-  //       if(err){
-  //         res.status(500).send(err);
-  //       } else {
-  //         res.json(req.resource);
-  //       }
-  //     })
-  //   })
-  //   .patch(function (req, res) {
-  //     if(req.body._id){
-  //       delete req.body._id;
-  //     }
-  //     for (p in req.body) {
-  //       req.resource[p] = req.body[p];
-  //     }
-  //     req.resource.save(function (err) {
-  //       if(err){
-  //         res.status(500).send(err);
-  //       } else {
-  //         res.json(req.resource);
-  //       }
-  //     })
-  //   })
-  //   .delete(function (req, res) {
-  //     req.resource.remove(function (err) {
-  //       if(err) {
-  //         res.status(500).send(err);
-  //       } else {
-  //         res.status(204).send('removed');
-  //       }
-  //     });
-  //   })
-  //   ;
+      req.resource.save(function (err) {
+        if(err){
+          res.status(500).send(err);
+        } else {
+          res.json(req.resource);
+        }
+      })
+    })
+    .patch(function (req, res) {
+      if(req.body._id){
+        delete req.body._id;
+      }
+      for (p in req.body) {
+        req.resource[p] = req.body[p];
+      }
+      req.resource.save(function (err) {
+        if(err){
+          res.status(500).send(err);
+        } else {
+          res.json(req.resource);
+        }
+      })
+    })
+    .delete(function (req, res) {
+      req.resource.remove(function (err) {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.status(204).send('removed');
+        }
+      });
+    })
+    ;
 
   return resourceRouter;
 };
